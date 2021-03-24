91aa416ab2f36f517b5e45692b073cf6bbf42fdd
S,S,Saruar Jahan <40640156+sjalim@users.noreply.github.com>
2021-03-24 22:54:15 +0600

    user profile done

--- controllers/profile.js
+++ controllers/profile.js
@@ -2,6 +2,7 @@
 
 const mysql = require("mysql");
 const jwt = require('jsonwebtoken');
+const {promisify} = require('util');
 
 
 const db = mysql.createConnection({
@@ -13,6 +14,59 @@ const db = mysql.createConnection({
 });
 
 
-exports.profile = async (res,req) => {
+exports.profileUpdate = async (req,res,next) => {
+
+    console.log("chek");
+    try{
+
+       const {name, email, countryCode, phone} = req.body;
+
+        if (req.cookies.jwt) {
+            try {
+
+                //verify the cookies
+                const decoded = await promisify(jwt.verify)(req.cookies.jwt, process.env.jwt_secret);
+
+                db.query('select * from users where id = ?', [decoded.id], (error, result) => {
+
+                    console.log(result);
+                    if (!result) {
+                        return next();
+                    }
+                    var sql = `UPDATE users SET name="${name}", country_code="${countryCode}", email="${email}",phone=${phone} WHERE id=${result[0].id}`;
+
+                    db.query(sql, function(err, result) {
+                        if (err) throw err;
+                        console.log('record updated!');
+                        res.redirect('/profile');
+                    });
+                });
+            } catch (error) {
+
+                console.log(error);
+                res.redirect('/profile');//pop up here
+
+            }
+        } else {
+
+            next();
+        }
+
+
+
+
+
+
+
+    }catch (e)
+    {
+        console.log(e);
+    }
+
+
+    // req.end();
+
+
 
 };
+
